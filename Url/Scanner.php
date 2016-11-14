be7d9383876f0d580adce646cfa096a0e8544eb2
M1racle <crazyhl@163.com>
2016-11-14 23:49:27 +0800

    init

---
+++ Url/Scanner.php
@@ -0,0 +1,38 @@
+<?php
+namespace Cimple\Scanner\Url;
+
+/**
+* Scanner
+*/
+class Scanner
+{
+	protected $urls;
+	protected $httpClient;
+	
+	public function __construct(array $urls)
+	{
+		$this->url = $urls;
+		$this->httpClient = new \GuzzleHttp\Client();
+	}
+
+	public function getInvalidUrls()
+	{
+		$invalidUrls = [];
+		foreach ($this->urls as $url) {
+			try {
+				$statusCode = $this->getStatusCodeForUrl($url);
+			} catch (\Exception $e) {
+				$statusCode = 500;
+			}
+
+			if ($statusCode >= 400) {
+				array_push($invalidUrls, [
+					'url' => $url,
+					'status' => $statusCode,
+				]);
+			}
+		}
+		
+		return $invalidUrls;
+	}
+}
